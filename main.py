daf88a7a1b71f4afd507367d9c50f8fd6351125b
Lennart Voelz <lennart.voelz@tu-dortmund.de>
2025-06-01 20:49:28 +0200

    Update data preprocessing and results calculation in main.py and truth_plots.ipynb

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import numpy as np
 
 config = yaml.safe_load(open('config.yaml'))
 
-data = DataPreprocessor(data_path=config['data_path'], raw_data_path=config['raw_data_path'], cuts=False, splits=False)
+data = DataPreprocessor(data_path=config['truth_path'], raw_data_path=config['raw_data_path'],
+                        cuts=False, splits=False, drop_zeroes=True)
 
 X, y, types = data.run_preprocessing()
 
@@ -15,5 +16,5 @@ X = X[:,:8]
 
 final_state = np.concatenate((X, y), axis=1)
 
-results_truth = calculate_results([final_state], ["Truth"], "Truth Detector Simulation", types)
+results_truth = calculate_results([final_state], ["Truth"], "Truth", types)
 results_truth.run("reports/truth_detector/")
